2ab870cb25e0e4e3e7e2d88e8f13d111fcda73ae
Eric McCarthy <eric@limulus.net>
2014-07-23 22:16:53 -0700

    Don't need this commented out code anymore.

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ window.addEventListener("DOMContentLoaded", function () {
     var svgRoot = new vecs.SVGRoot(400, 400)
     svgRoot.addAt(rect, 100, 100)
     svgRoot.addToDocumentBodyWithId("logo")
-    // svgRoot.setPreserveAspectRatio("none")
-    // svgRoot.setViewBox("0 0 1500 1500")
 
     svgRoot.observe("click", function (e) {
         rect.setCoords(e.positionRelativeToViewport())
