bed122ce8a9a82f92072e11a715f67912b535964
Eric McCarthy <eric@limulus.net>
2014-09-18 13:34:16 -0700

    Gesture handling? Committing old uncomitted code.

--- vecs/SVGRoot.js
+++ vecs/SVGRoot.js
@@ -6,6 +6,9 @@ var SVGElement = require("./SVGElement.js")
 
 var SVGRoot = module.exports = function (width, height) {
     SVGElement.call(this, "svg")
+
+    this._isObservingTouchGesture = false
+
     this.rawSVGElement().setAttribute("version", "1.1")
     this.rawSVGElement().setAttribute("width", width + "px")
     this.rawSVGElement().setAttribute("height", height + "px")
@@ -35,19 +38,29 @@ SVGRoot.prototype.setPreserveAspectRatio = function (val) {
 }
 
 SVGRoot.prototype.observeTouchGesture = function (eventHandler) {
-    var _handleEvent = function (e) {
+    var beginGestureHandler = function (e) {
+        e.preventDefault()
+        eventHandler(new SVGEvent(e))
+    }
+
+    var continueGestureHandler = function (e) {
+        e.preventDefault()
+        eventHandler(new SVGEvent(e))
+    }
+
+    var endGestureHandler = function (e) {
         e.preventDefault()
         eventHandler(new SVGEvent(e))
     }
 
     if (document.ontouchstart === undefined) {
-        this.rawSVGElement().addEventListener("mousedown", _handleEvent, false)
-        this.rawSVGElement().addEventListener("mousemove", _handleEvent, false)
-        this.rawSVGElement().addEventListener("mouseup", _handleEvent, false)
+        this.rawSVGElement().addEventListener("mousedown", beginGestureHandler, false)
+        this.rawSVGElement().addEventListener("mousemove", continueGestureHandler, false)
+        this.rawSVGElement().addEventListener("mouseup", endGestureHandler, false)
     }
     else {
-        this.rawSVGElement().addEventListener("touchstart", _handleEvent, false)
-        this.rawSVGElement().addEventListener("touchmove", _handleEvent, false)
-        this.rawSVGElement().addEventListener("touchend", _handleEvent, false)
+        this.rawSVGElement().addEventListener("touchstart", beginGestureHandler, true)
+        this.rawSVGElement().addEventListener("touchmove", continueGestureHandler, true)
+        this.rawSVGElement().addEventListener("touchend", endGestureHandler, true)
     }
 }
